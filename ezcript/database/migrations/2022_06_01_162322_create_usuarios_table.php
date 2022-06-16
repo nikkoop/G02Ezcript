972d06c17ffe10b5630dde43885bc1d4c21d1f31
SantiagoVillablanca <93687511+SantiagoVillablanca@users.noreply.github.com>
2022-06-15 20:36:00 -0400

    migraciones de las tablas Usuario y Rol

---
+++ ezcript/database/migrations/2022_06_01_162322_create_usuarios_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+
+            $table->id('pef_id');
+
+            $table->string('pef_foto');
+            $table->string('pef_rut');
+            $table->string('pef_nombre');
+            $table->string('pef_apellido_paterno');
+            $table->string('pef_apellido_materno');
+            $table->string('pef_correo');
+            $table->string('pef_telefono');
+            $table->string('pef_pagina_web');
+            $table->string('pef_descripcion');
+            $table->string('pef_contrasena');
+
+            $table->foreignId('rol_id')->references('rol_id')->on('rol');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+};
