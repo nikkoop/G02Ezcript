a83bc49eaa8d6155d1227725556702b7d841c627
Evitha-CS <evelyn.cariaga1801@alumnos.ubiobio.cl>
2022-07-27 23:15:47 -0400

    Solución tablas de curso y actualización composer

--- ezcript/database/migrations/2022_05_28_183806_create_periodo_table.php
+++ ezcript/database/migrations/2022_05_28_183806_create_periodo_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('periodo', function (Blueprint $table) {
-            $table->integer('id')->primary();
+            $table->id();
             $table->string('per_anio');
             $table->string('per_semestre');
             $table->timestamps();
